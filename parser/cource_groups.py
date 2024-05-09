264f60c4adb6b8df87f9003e05b095a69c94e047
DiHASTRO <d87779007501d@gmail.com>
2024-05-09 12:18:33 +0500

    Refactor: switch to dataclasses

--- parser/cource_groups.py
+++ parser/cource_groups.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import requests
 from typing import List
 
@@ -6,37 +7,16 @@ from bs4 import BeautifulSoup
 from config import config
 
 
+@dataclass
 class Group:
     name: str
     key: int
 
-    def __init__(self, name: str, key: int):
-        self.name = name
-        self.key = key
-    
-    def __str__(self) -> str:
-        return f"Group(name={self.name}, key={self.key})"
-    
-    def __repr__(self) -> str:
-        return self.__str__()
 
-
+@dataclass
 class CourseGroups:
     name: str
     groups: List[Group]
-    
-    def __init__(self, course_name: str):
-        self.name = course_name
-        self.groups = []
-    
-    def add_group(self, group: Group):
-        self.groups.append(group)
-    
-    def __str__(self) -> str:
-        return f"CourseGroups(name={self.name}, groups=[" + ", ".join(map(str, self.groups)) + "])"
-
-    def __repr__(self) -> str:
-        return self.__str__()
 
 
 def get(faculty_href: str) -> List[CourseGroups]:
@@ -47,7 +27,9 @@ def get(faculty_href: str) -> List[CourseGroups]:
 
     arr: List[CourseGroups] = []
     for course in courses:
-        arr.append(CourseGroups(course.find('h3').text))
+        arr.append(
+            CourseGroups(course.find('h3').text, []),
+        )
         groups = course.find_all(class_='groups-list__item')
         for group in groups:
             group = group.find('a')
