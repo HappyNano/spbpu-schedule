05617e93695b33e7c14188d0c9ebd99cf372eb6c
Alexey <akurzov41@gmail.com>
2024-05-09 23:22:04 +0300

    Corrected some errors in schedule parser

--- schedule/tools.py
+++ schedule/tools.py
@@ -9,17 +9,9 @@ def getData(url: str) -> json:
         return (client.get(url=url)).json()
 
 def is_groupId_correct(group_id: int) -> bool:
-    loop = asyncio.get_event_loop()
     try:
         url = config.SCHEDULE_URL.format(group_id)
-        data = loop.run_until_complete(asyncio.gather(getData(url)))[0]
+        data = getData(url)
         return data['error'] is None
     except:
         return True
-
-def is_int(text: str) -> bool:
-    try:
-        int(text)
-        return True
-    except:
-        return False
